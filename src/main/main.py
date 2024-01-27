6b8687ff5df7a37663c7f12719680f114d75e32d
MateoVG-coding <mateovega10@hotmail.com>
2024-01-27 09:55:48 -0500

    Add setup in main for database.

--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///src/db/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
 
-@app.route("/")
-def hello_world():
-    """Basic main function"""
-    return "Hello, World!"
+if __name__ == '__main__':
+    app.run(debug=True)
