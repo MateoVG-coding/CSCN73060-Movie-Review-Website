77d801fa4d6b7ba45f4f0b93b192a3fcdeefa0db
MateoVG-coding <mateovega10@hotmail.com>
2024-01-28 21:07:02 -0500

    Add blueprint to main app.

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ from models import connect_to_db, db
 from login_routes import login_bp
 from register_routes import register_bp
 from review_route import review_bp
+from homepage_route import home_bp
+from movies_routes import movies_bp
 
 app = Flask(__name__)
 
@@ -15,5 +17,7 @@ if __name__ == "__main__":
     app.register_blueprint(login_bp)
     app.register_blueprint(register_bp)
     app.register_blueprint(review_bp)
+    app.register_blueprint(home_bp)
+    app.register_blueprint(movies_bp)
 
     app.run(debug=True)
